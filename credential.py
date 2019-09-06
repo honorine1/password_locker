35f27480122428ed20565aa721c5d79e1f5ab9ab
honorine1 <uwurukundoh@gmail.com>
2019-09-06 10:41:16 +0200

    credentials class and its content is added

--- credential.py
+++ credential.py
@@ -2,11 +2,66 @@ class Credential:
     '''
     a class that generates new credential instance
     '''
-    credential_list = [] 
-    def __init__(self,cred_acc,username,password):
+    cred_list = [] 
+    ##################initialization test###########
+    def __init__(self,acc_name,acc_username,acc_password):
         '''
         this is a method is showing the properties of the above method
         '''
-        self.cred_acc = cred_acc
-        self.username = username
-        self.password = password
+        self.acc_name = acc_name
+        self.acc_username = acc_username
+        self.acc_password = acc_password
+
+        #######3to save the credential####
+    def save_credential(self):
+
+        '''
+        save_contact method saves credential objects into cred_list
+        '''
+
+        Credential.cred_list.append(self)
+
+        ##########3to display the saved credentials#######
+    @classmethod
+    def display_credentials(cls):
+        '''
+        method that returns the cred_list
+        '''
+        return cls.cred_list
+
+        #########to find a given credential using acc_name#######
+
+    @classmethod
+    def find_by_name(cls,acc_name):
+        '''
+        Method that takes in a site name and returns a credential that matches that site name.
+
+        '''
+
+        for credential in cls.cred_list:
+            if credential.acc_name == acc_name:
+                return credential
+       
+
+        ##########method to generate password#######
+
+    @classmethod
+    def generate_password(self):
+        '''
+        A method to generate a credential generate_password
+        '''
+
+#         @classmethod
+#    def passGenerator(cls):
+#        '''
+#        method that returns the a random password
+#        '''
+    #    s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    #    passlen = 8
+    #    p =  "".join(random.sample(s,passlen ))
+    #    return p
+
+        passwordlen = 8
+        s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?0123456789"
+        gen_password=''.join(random.choice(s) for _ in range(8))
+        return gen_pass
--- credential_test.py
+++ credential_test.py
@@ -11,11 +11,80 @@ class TestCredential(unittest.TestCase):
          '''
          Set up method to run before each test cases.
          '''
-         self.new_credential = Credential ('keza joy','kjoy','kezajoymami2020') #create user object
+         self.new_credential = Credential ('facebook','mimy','111') #create user object
 
 
      def tearDown(self):
          '''
          tearDown method that does clean up after each test case has run.
          '''
-         User.users_list = []
+         Credential.cred_list = []
+
+         ################################3
+
+     def test_init(self):
+         '''
+         test_init test case to test if the object is initialized properly
+         '''
+
+         self.assertEqual(self.new_credential.acc_name,"facebook")
+         self.assertEqual(self.new_credential.acc_username,"mimy")
+         self.assertEqual(self.new_credential.acc_password,"111")
+
+         #########################to save credentials###########
+     def test_save_credential(self):
+         '''
+         test_save_contact test case to test if the credential object is saved into
+         the cred_list
+         '''
+         self.new_credential.save_credential()
+         self.assertEqual(len(Credential.cred_list),1)
+
+         #########3save multiple credentials######
+
+
+     def test_save_multiple_credential(self):
+         '''
+         test_save_multiple_credential to check if we can save multiple credentials
+         objects to our cred_list
+         '''
+         self.new_credential.save_credential()
+         test_credential = Credential("facebook","mimy","111") 
+         test_credential.save_credential()
+         self.assertEqual(len(Credential.cred_list),2)
+
+         #####to display the alredy saved credential#####
+
+     def test_display_all_credentials(self):
+         '''
+         test to chech if all credential can be displayed
+         '''
+         self.assertEqual(Credential.display_credentials(),Credential.cred_list)
+
+         ######find by acc_name#######
+
+     def test_find_contact_by_name(self):
+         '''
+         test to check if we can find a credential by account_name and display information
+         '''
+
+         self.new_credential.save_credential()
+         test_credential = Credential("facebook","mimy","111") 
+         test_credential.save_credential()
+         
+         get_credential = Credential.find_by_name("facebook")
+         self.assertEqual(get_credential.acc_username,test_credential.acc_username)
+
+       ##########generating password test##############
+
+       
+     def test_generate_password(self):
+        '''
+        test_generate_password test case to test if a given account has its own password
+        '''
+        self.new_credential.generate_password()
+        self.assertEqual(Credential.display_credentials(),Credential.cred_list)
+
+
+if __name__ == '__main__':
+      unittest.main()
